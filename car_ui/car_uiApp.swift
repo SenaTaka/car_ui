7319ff75c46f2a272022910add89f0a2aa972387
SenaTaka <st0327t@gmail.com>
2026-02-20 22:59:35 +0900

    Initial Commit

---
+++ car_ui/car_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_uiApp.swift
+//  car_ui
+//
+//  Created by Sena Takasawa on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct car_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
